9e45d92f9b27257df10d1a924045dc79888f8e92
Marcin Gólski <marcin.golski@student.put.poznan.pl>
2024-03-02 19:56:49 +0100

    Random permutation of fixed-size array

---
+++ src/bin/my_random.rs
@@ -0,0 +1,31 @@
+use rand::prelude::*;
+// use rand::{Rng, SeedableRng};
+
+
+fn random_permutation<const LENGTH: usize>() -> [i32;LENGTH] {
+    let mut array: [i32; LENGTH] = [0; LENGTH];
+    let mut rng: ThreadRng = rand::thread_rng();
+
+    for i in 0..LENGTH {
+        let val: i32 = i as i32;
+        array[i] = val;
+    }
+    for i in 0..LENGTH {
+        let src_idx: usize = rng.gen_range(0..LENGTH-i);
+        let target_idx: usize = LENGTH - i - 1;
+        let tmp = array[src_idx];
+        array[src_idx] = array[target_idx];
+        array[target_idx] = tmp;
+    }
+    return array;
+}
+
+fn main() {
+    const LENGTH: usize = 100;
+    // let mut rng = seed_from_u64(10);
+    // println!("Random i32: {}", rng.gen::<i32>(10));
+    let arr = random_permutation::<LENGTH>();
+    for val in arr.iter() {
+        println!("{}", val);
+    }
+}
