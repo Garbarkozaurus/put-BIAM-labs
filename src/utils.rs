f289d8e4d3cb64a2bccac7baf518d746ee6231b9
Marcin Gólski <marcin.golski@student.put.poznan.pl>
2024-03-10 18:43:55 +0100

    Make basic_evaluate take references as arguments

--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,14 @@
 use crate::QapInstance;
 use crate::QapSolution;
 
-pub fn basic_evaluate(instance: QapInstance, solution: QapSolution) -> u32 {
+pub fn basic_evaluate(instance: &QapInstance, solution: &QapSolution) -> u32 {
     let mut cost: u32 = 0;
     for i in 0..instance.instance_size {
         for j in 0..instance.instance_size {
             let row: usize = i as usize;
             let col: usize = j as usize;
             cost += instance.costs[row][col]
-                * instance.interactions[solution.assignments[row] as usize]
-                    [solution.assignments[col] as usize];
+                * instance.interactions[solution.assignments[row]][solution.assignments[col]];
         }
     }
     cost
