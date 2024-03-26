05ea248bcf96aa6e98036c5b09955941e4282d53
Marcin Gólski <marcin.golski@student.put.poznan.pl>
2024-03-26 19:53:15 +0100

    Correct calculation of swap delta

--- src/utils.rs
+++ src/utils.rs
@@ -13,3 +13,50 @@ pub fn basic_evaluate(instance: &QapInstance, solution: &QapSolution) -> u32 {
     }
     cost
 }
+
+pub fn swap_delta(
+    instance: &QapInstance,
+    solution: &QapSolution,
+    idx_a: usize,
+    idx_b: usize,
+) -> i32 {
+    // likely to be the hottest function, optimise carefully!
+    let mut old_subcost: u32 = 0;
+    let mut new_subcost: u32 = 0;
+    let n: usize = instance.instance_size as usize;
+    // immutable reference
+    let sol: &[usize] = &solution.assignments[..n];
+    // This cloning operation might take a while, but it makes
+    // the code easier to follow (and within my ability to write it)
+    let mut sol2: [usize; 256] = solution.assignments.clone();
+    sol2.swap(idx_a, idx_b);
+    // Impacts only rows and columns idx_a and idx_b of A
+    // the rows are multiplied by B in the order of teh permutation
+    for i in 0..n {
+        // This block handles columns of the "swapped rows"
+        old_subcost += instance.costs[idx_a][i] * instance.interactions[sol[idx_a]][sol[i]];
+        old_subcost += instance.costs[idx_b][i] * instance.interactions[sol[idx_b]][sol[i]];
+        new_subcost += instance.costs[idx_a][i] * instance.interactions[sol2[idx_a]][sol2[i]];
+        new_subcost += instance.costs[idx_b][i] * instance.interactions[sol2[idx_b]][sol2[i]];
+
+        // This block handles rows of the "swapped columns"
+        old_subcost += instance.costs[i][idx_a] * instance.interactions[sol[i]][sol[idx_a]];
+        old_subcost += instance.costs[i][idx_b] * instance.interactions[sol[i]][sol[idx_b]];
+
+        new_subcost += instance.costs[i][idx_a] * instance.interactions[sol2[i]][sol2[idx_a]];
+        new_subcost += instance.costs[i][idx_b] * instance.interactions[sol2[i]][sol2[idx_b]];
+    }
+    // the multiplications at the intersections of the "swapped rows and columns"
+    // are counted twice. This is corrected here
+    old_subcost -= instance.costs[idx_a][idx_a] * instance.interactions[sol[idx_a]][sol[idx_a]];
+    old_subcost -= instance.costs[idx_a][idx_b] * instance.interactions[sol[idx_a]][sol[idx_b]];
+    old_subcost -= instance.costs[idx_b][idx_a] * instance.interactions[sol[idx_b]][sol[idx_a]];
+    old_subcost -= instance.costs[idx_b][idx_b] * instance.interactions[sol[idx_b]][sol[idx_b]];
+
+    new_subcost -= instance.costs[idx_a][idx_a] * instance.interactions[sol2[idx_a]][sol2[idx_a]];
+    new_subcost -= instance.costs[idx_a][idx_b] * instance.interactions[sol2[idx_a]][sol2[idx_b]];
+    new_subcost -= instance.costs[idx_b][idx_a] * instance.interactions[sol2[idx_b]][sol2[idx_a]];
+    new_subcost -= instance.costs[idx_b][idx_b] * instance.interactions[sol2[idx_b]][sol2[idx_b]];
+
+    new_subcost as i32 - old_subcost as i32
+}
