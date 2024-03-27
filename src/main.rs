3c7acd207f542ba2af9a9cf22cf67f189180c2bc
Marcin Gólski <marcin.golski@student.put.poznan.pl>
2024-03-27 13:01:22 +0100

    Steepest LS using deltas

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod qap_instance;
+pub mod search_monitor;
 use qap_instance::QapInstance;
 pub mod qap_solution;
 use qap_solution::QapSolution;
@@ -6,28 +7,12 @@ pub mod steepest;
 pub mod utils;
 use std::env;
 
-use crate::utils::{basic_evaluate, swap_delta};
-
 pub const MAX_INSTANCE_SIZE: usize = 256;
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
-    const PATH: &str = "./qap_data/tai10a.dat";
-    let example_instance: QapInstance = QapInstance::instance_from_file(PATH);
-
-    let mut sol: QapSolution = steepest::simplest_steepest_local_search(&example_instance);
-    let old_eval = basic_evaluate(&example_instance, &sol) as i32;
-    let a = 0;
-    let b = 2;
-    let delta = swap_delta(&example_instance, &sol, a, b);
-    sol.assignments.swap(a, b);
-    let new_eval = basic_evaluate(&example_instance, &sol) as i32;
-    println!("OG eval: {}", old_eval);
-    println!("Delta: {}", delta);
-    println!(
-        "Estimated new: {}\tActual new: {}\t Error: {}",
-        old_eval + delta,
-        new_eval,
-        old_eval + delta - new_eval
-    )
+    const INSTANCE_NAME: &str = "tai256c";
+    let path: String = format!("qap_data/{INSTANCE_NAME}.dat");
+    let example_instance: QapInstance = QapInstance::instance_from_file(&path);
+    steepest::deltas_steepest_local_search(&example_instance, 0, INSTANCE_NAME);
 }
--- src/steepest.rs
+++ src/steepest.rs
@@ -1,17 +1,21 @@
+use crate::search_monitor::SearchMonitor;
+use crate::utils;
 use crate::utils::basic_evaluate;
+use crate::utils::swap_delta;
 use crate::QapInstance;
 use crate::QapSolution;
 use crate::MAX_INSTANCE_SIZE;
 use rand::prelude::ThreadRng;
 use rand::Rng;
 use std::i32::MAX;
+use std::time;
 
 fn naive_steepest_best_swap(instance: &QapInstance, solution: &QapSolution) -> (usize, usize, i32) {
     let mut best_delta: i32 = MAX;
     let mut neighbors_with_best_delta: usize = 0;
     let mut best_swaps: [[usize; 2]; MAX_INSTANCE_SIZE * MAX_INSTANCE_SIZE] =
         [[0, 0]; MAX_INSTANCE_SIZE * MAX_INSTANCE_SIZE];
-    let original_eval = basic_evaluate(&instance, &solution) as i32;
+    let original_eval = utils::basic_evaluate(&instance, &solution) as i32;
     for i in 0..(instance.instance_size - 1) {
         for j in i + 1..(instance.instance_size) {
             let mut swapped_order = solution.assignments.clone();
@@ -20,7 +24,7 @@ fn naive_steepest_best_swap(instance: &QapInstance, solution: &QapSolution) -> (
                 instance_size: instance.instance_size,
                 assignments: swapped_order,
             };
-            let new_eval = basic_evaluate(&instance, &new_solution) as i32;
+            let new_eval = utils::basic_evaluate(&instance, &new_solution) as i32;
             let delta = new_eval - original_eval;
             if delta == best_delta {
                 best_swaps[neighbors_with_best_delta][0] = i;
@@ -49,5 +53,77 @@ pub fn simplest_steepest_local_search(instance: &QapInstance) -> QapSolution {
         }
         starting_solution.assignments.swap(a, b);
     }
+    starting_solution
+}
+
+/// Finds the swap which maximizes the improvement
+///
+/// Returns: swap_idx1, swap_idx2, delta
+fn delta_steepest_best_swap(
+    instance: &QapInstance,
+    solution: &QapSolution,
+    monitor: &mut SearchMonitor,
+) -> (usize, usize, i32) {
+    let mut best_delta: i32 = MAX;
+    let mut neighbors_with_best_delta: usize = 0;
+    let mut best_swaps: [[usize; 2]; MAX_INSTANCE_SIZE * MAX_INSTANCE_SIZE] =
+        [[0, 0]; MAX_INSTANCE_SIZE * MAX_INSTANCE_SIZE];
+    for i in 0..(instance.instance_size - 1) {
+        for j in i + 1..(instance.instance_size) {
+            let delta: i32 = swap_delta(instance, solution, i, j);
+            monitor.num_evaluations += 1;
+            if delta == best_delta {
+                best_swaps[neighbors_with_best_delta][0] = i;
+                best_swaps[neighbors_with_best_delta][1] = j;
+                neighbors_with_best_delta += 1;
+            }
+            if delta < best_delta {
+                best_swaps[0][0] = i;
+                best_swaps[0][1] = j;
+                best_delta = delta;
+                neighbors_with_best_delta = 1;
+            }
+        }
+    }
+    let mut rng: ThreadRng = rand::thread_rng();
+    let best_swap: [usize; 2] = best_swaps[rng.gen_range(0..neighbors_with_best_delta)];
+    (best_swap[0], best_swap[1], best_delta)
+}
+
+pub fn deltas_steepest_local_search(
+    instance: &QapInstance,
+    run_id: u32,
+    instance_name: &str,
+) -> QapSolution {
+    let mut monitor: SearchMonitor = SearchMonitor {
+        run_id,
+        instance_name: instance_name.to_string(),
+        search_type: "steepest".to_string(),
+        instance_size: instance.instance_size,
+        num_visited_solutions: 0,
+        num_evaluations: 0,
+        running_time_ms: 0,
+        best_assignments: [0; MAX_INSTANCE_SIZE],
+        cost_history: vec![],
+    };
+    let start: time::Instant = time::Instant::now();
+    let mut starting_solution: QapSolution = QapSolution::random_solution(instance.instance_size);
+    let mut cost: u32 = basic_evaluate(&instance, &starting_solution);
+    monitor.cost_history.push(cost);
+    loop {
+        let (idx_a, idx_b, delta) =
+            delta_steepest_best_swap(&instance, &starting_solution, &mut monitor);
+        if delta >= 0 {
+            break;
+        }
+        starting_solution.assignments.swap(idx_a, idx_b);
+        monitor.num_visited_solutions += 1;
+        cost = ((cost as i32) + delta) as u32;
+        monitor.cost_history.push(cost);
+    }
+    monitor.best_assignments = starting_solution.assignments;
+    let duration: time::Duration = start.elapsed();
+    monitor.running_time_ms = duration.as_millis() as u32;
+    monitor.export_to_files();
     starting_solution
 }
