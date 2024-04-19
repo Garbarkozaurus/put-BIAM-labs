b12f8b30dd69178d14da4808c97f718ec1078ce0
Marcin Gólski <marcin.golski@student.put.poznan.pl>
2024-04-19 10:41:10 +0200

    Add SA to main, \r for showing progress

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ pub mod qap_instance;
 pub mod random_search;
 pub mod random_walk;
 pub mod search_monitor;
+pub mod simulated_annealing;
+pub mod tabu_search;
+
+use std::io::stdout;
+use std::io::Write;
+
 use qap_instance::QapInstance;
 pub mod qap_solution;
 use qap_solution::QapSolution;
@@ -20,16 +26,23 @@ fn main() {
     let timeouts: [u32; 12] = [
         100, 150, 200, 700, 1600, 2600, 5200, 12500, 13000, 35000, 240000, 1500000,
     ];
-    for num in 0..12 {
+    let num_instances: usize = 12;
+    let num_runs: u32 = 500;
+    let mut stdout: std::io::Stdout = stdout();
+    for num in 0..num_instances {
         let instance_name: &str = instance_names[num];
         let timeout: u32 = timeouts[num];
         let path: String = format!("qap_data/{instance_name}.dat");
         let example_instance: QapInstance = QapInstance::instance_from_file(&path);
-        for i in 0..100 {
+        println!("=== {} ===", instance_name);
+        for i in 0..num_runs {
+            print!("{i}/{num_runs}\r");
+            stdout.flush().unwrap();
             steepest::deltas_steepest_local_search(&example_instance, i, instance_name);
             greedy::deltas_greedy_local_search(&example_instance, i, instance_name);
             random_search::random_search(&example_instance, i, instance_name, timeout);
             random_walk::random_walk(&example_instance, i, instance_name, timeout);
+            simulated_annealing::deltas_simulated_annealing(&example_instance, i, instance_name);
         }
     }
 }
